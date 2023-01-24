9c1bde85c79eda72e6d0f2a7c0d9e2d87cec5d07
Đinh Bách Thông <thongdb.dev@gmail.com>
2023-01-24 10:42:50 +0700

    Building Conversation Component

--- src/pages/dashboard/GeneralApp.js
+++ src/pages/dashboard/GeneralApp.js
@@ -1,12 +1,28 @@
 import React from "react";
+import { Box, Stack } from "@mui/material";
+import { useTheme } from "@mui/material/styles";
 import Chats from "./Chats";
+import Conversation from "../../components/Conversation";
 
 const GeneralApp = () => {
+  const theme = useTheme();
 
   return (
-    <>
+    <Stack direction="row">
       <Chats />
-    </>
+      <Box
+        sx={{
+          height: "100%",
+          width: "calc(100vw - 420px)",
+          backgroundColor:
+            theme.palette.mode === "light"
+              ? "#fff"
+              : theme.palette.background.default,
+        }}
+      >
+        <Conversation />
+      </Box>
+    </Stack>
   );
 };
 
