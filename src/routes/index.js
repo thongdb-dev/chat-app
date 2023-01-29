19522e27b07fc202ec0ca8ad6e4e58c79fd73855
Đinh Bách Thông <thongdb.dev@gmail.com>
2023-01-29 12:13:03 +0700

    Building Login Page

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,7 @@ import { Navigate, useRoutes } from "react-router-dom";
 
 // layouts
 import DashboardLayout from "../layouts/dashboard";
+import MainLayout from "../layouts/main";
 
 // config
 import { DEFAULT_PATH } from "../config";
@@ -18,6 +19,13 @@ const Loadable = (Component) => (props) => {
 
 export default function Router() {
   return useRoutes([
+    {
+      path: "/auth",
+      element: <MainLayout />,
+      children: [
+        { path: "login", element: <LoginPage /> },
+      ]
+    },
     {
       path: "/",
       element: <DashboardLayout />,
@@ -38,8 +46,8 @@ const GeneralApp = Loadable(
   lazy(() => import("../pages/dashboard/GeneralApp"))
 );
 
-const Settings = Loadable(
-  lazy(() => import("../pages/dashboard/Settings"))
-);
+const Settings = Loadable(lazy(() => import("../pages/dashboard/Settings")));
+
+const LoginPage = Loadable(lazy(() => import("../pages/auth/Login")));
 
 const Page404 = Loadable(lazy(() => import("../pages/Page404")));
